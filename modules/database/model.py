e85db18b9fd25f1fef77c6966b175d2e25f2097c
BarsTiger <kit.barsic@gmail.com>
2022-08-03 22:45:12 +0300

    removed test database with cringe data from model.py

--- modules/database/model.py
+++ modules/database/model.py
@@ -32,35 +32,3 @@ default_database = {
     "items": None,
     "profiles": None
 }
-
-
-aa = DatabaseModel.from_dict(
-    {
-        "items": {
-            "sieg": {
-                "item_name": "seig",
-                "item_class": "meow",
-                "item_type": "bebra44ka",
-                "shop": "aaaaaaatb",
-                "price": 100000000,
-                "image": "https://magichitler.sieg/public/static/img/mama.png"
-            },
-            "aaaaaaaaaaaaa": {
-                "item_name": "aaaaaaaaaaaaa",
-                "item_class": "aaaaaaaaaaaaaaaaaaaa",
-                "item_type": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-                "shop": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-                "price": 10000000000000000,
-                "image": "https://sieg-heil.com/"
-            }
-        },
-        "profiles": {
-            "BarsTiger": {
-                "profile_name": "BarsTiger",
-                "owned_items": [
-                    "aaaaaaaaaaaaa"
-                ]
-            }
-        }
-    }
-)
